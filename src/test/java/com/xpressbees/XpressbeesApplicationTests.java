e962915d8332dff90535beb99009a005285efe85
Anshul Garg - a0g11b6 <Anshul.Garg0@walmart.com>
2025-02-03 04:37:33 +0530

    test

---
+++ src/test/java/com/xpressbees/XpressbeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpressbees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpressbeesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
